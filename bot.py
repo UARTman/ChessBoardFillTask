ac5a2243dde18e82305e61885a385fd3b20e33ec
UARTman <agus179e@gmail.com>
2019-05-13 14:52:32 +0300

    Added simple bot: bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 14:37:13 2019
+
+@author: UARTman
+"""
+
+def make_turn(field):
+    variants={}
+    r = []
+    for i in range(len(field)):
+        for j in range(len(field)):
+            if field[i][j] == 0:
+                variants[(i, j)] = 0
+    for q in variants:
+        for i in range(len(field)):
+            if i != q[0] and field[i,j] == 0:
+                variants[q] += 1
+            if i != q[1] and field[i,j] == 0:
+                variants[q] += 1
+    for i in variants:
+        r.append(variants[i])
+    for i in variants:
+        if variants[i] == max(r):
+            return [i[0], i[1], 4]
+    return [-1, -1, 4]
