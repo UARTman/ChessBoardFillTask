98f15a543a6c23b307f35944d8d87cfb2020b763
UARTman <agus179e@gmail.com>
2019-05-13 15:00:15 +0300

    Updated bot.py

--- bot.py
+++ bot.py
@@ -14,9 +14,9 @@ def make_turn(field):
                 variants[(i, j)] = 0
     for q in variants:
         for i in range(len(field)):
-            if i != q[0] and field[i,j] == 0:
+            if i != q[0] and field[i][q[1]] == 0:
                 variants[q] += 1
-            if i != q[1] and field[i,j] == 0:
+            if i != q[1] and field[q[0]][i] == 0:
                 variants[q] += 1
     for i in variants:
         r.append(variants[i])
