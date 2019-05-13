d1411184a9a8bda1733072c2f66c8a020ea3c6b8
Your Name <gadago@yandex.ru>
2019-05-13 14:41:10 +0300

    add upload() and end generation(a, b)

--- generation.py
+++ generation.py
@@ -1,2 +1,12 @@
 def generation(a, b):
-    return 0
+    field = []
+    for i in range(a):
+        field.append([0 for k in range(b)])
+    return field
+def upload():
+    f = open('settings.txt', 'r')
+    temp = []
+    str_a = f.read(1)
+    s = f.read(1)
+    str_b = f.read(1)
+    return int(str_a), int(str_b)
