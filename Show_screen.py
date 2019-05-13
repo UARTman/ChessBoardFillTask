e4c89110465e6a8e5f22c7149d3b45458bb9480b
Maxim <maximkostrov@mail.ru>
2019-05-13 14:36:19 +0300

    Changed file

--- Show_screen.py
+++ Show_screen.py
@@ -1,4 +1,11 @@
 def Show_screen(Field):
-
-
+    print('  abcdefgh')
+    for i in range(len(Field)):
+        print(i+1, '', end = '')
+        for k in Field[i]:
+            print(k, end = '')
+        print()
     return True
+
+
+Show_screen([[0, 0, 0], [1, 1, 1], [1, 2, 3]])
