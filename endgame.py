465be1f67f38e3f51585a3609f6546a9396038ed
Irashchedr <irishka2002@gmail.com>
2019-05-13 14:17:56 +0300

    Two functions created check symbol and end_of_game

---
+++ endgame.py
@@ -0,0 +1,13 @@
+def check_symbol(mas,symb,j1,i1): #наличие нужного символа в заданной ячейке
+    if mas[j1][i1] == symb:
+        return 0
+    else:
+        return 1
+def end_of_game (field,symb,n_row,n_col):#array,symbol of empty field, number of rows, number of columns
+    a=1
+    if a=0:
+        return True
+    else:
+        return False
+
+
